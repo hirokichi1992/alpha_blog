265b4f75002d6e3c5247640fc46f578b89740b17
hirokichi1992 <plastar14105@gmail.com>
2018-11-18 09:05:37 +0000

    Add pagination to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def gravatar_for(user, option = { size: 80})
+  def gravatar_for(user, options = { size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = option[:size]
+    size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt:user.username, class: "img-circle")
+    image_tag(gravatar_url, alt:'', class: "img-circle")
   end
 end
