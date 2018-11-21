d8c66b20f2d7146902cb15ab349de8b2cb42732b
hirokichi1992 <plastar14105@gmail.com>
2018-11-21 22:36:08 +0000

    Complete authntication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   
   config.time_zone = 'Tokyo'
   
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger]="You must be logged in to perform action"
+      redirect_to root_path
+    end
+  end
+  
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController < ApplicationController
+  
+  def new
+  
+  end
+  
+  def create
+    email = params[:sessions][:email].downcase
+    password = params[:sessions][:password]
+    user = User.find_by(email: email)
+    if login(email, password)
+      flash[:success] = "Hi! You has successfully logged in  Your mail-address is #{params[:sessions][:email]} #{params[:sessions][:password]} #{session[:user_id]} "
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login infomation"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to root_path
+  end
+  
+  private
+
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
+  end
+end
