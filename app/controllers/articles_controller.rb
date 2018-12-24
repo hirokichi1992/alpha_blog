d34723addf9a8486936dc856bbb2f80460535b84
hirokichi1992 <plastar14105@gmail.com>
2018-12-24 12:58:23 +0900

    記事作成時のカテゴリーの選択機能追加（チェックボックス）

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,15 +2,15 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :destroy, :edit, :update]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only:[:edit, :update, :destroy]
-  
+
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
     #render plain: params[:article].inspect //userのpost内容を確認
     #debugger //gem byebug で使えるデバッグ用 ここでとまる 進むときは「n」 抜けるときは「control d」
@@ -23,19 +23,19 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
   end
-  
+
   def destroy
     @article.destroy
     flash[:danger] = "Article was successflly deleted"
     redirect_to articles_path
   end
-  
+
   def edit
   end
-  
+
   def update
     if @article.update(article_params)
       flash[:success] = "Article was successfly updated"
@@ -44,15 +44,15 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
-  
+
     def set_article
       @article = Article.find(params[:id])
     end
-    
+
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
     
     def require_same_user
@@ -61,5 +61,5 @@ class ArticlesController < ApplicationController
         redirect_to root_path
       end
     end
-  
+
 end
